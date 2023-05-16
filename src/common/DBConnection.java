f58b9520a513776b02da2749189cc4d9a7a231b5
swalri <keb1128ke@gmail.com>
2023-05-16 15:03:31 +0900

    0516

--- src/common/DBConnection.java
+++ src/common/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
 	public static Connection getConnection() throws Exception {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		System.out.println("연결중");
-		String id = "C##java", pwd = "1234", url = "jdbc:oracle:thin:@localhost:1521/xe";
+		String id = "java", pwd = "1234", url = "jdbc:oracle:thin:localhost:1521/xe";
 		Connection con = DriverManager.getConnection(url, id, pwd);
 		System.out.println("성공");
 		return con;
